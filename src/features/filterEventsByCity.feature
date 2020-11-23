282d06270a825335156487b8b371368cf6a1c781
dill337 <64544519+dill337@users.noreply.github.com>
2020-11-23 16:40:46 -0600

    filterevents by city feature testing

--- src/features/filterEventsByCity.feature
+++ src/features/filterEventsByCity.feature
@@ -1,7 +1,7 @@
 Feature: Filter events by city
 
-  Scenario: When user hasn't searched for a city, show upcoming ecvenrts from all cities
-    Given user hasn't searched for any city
+  Scenario: When user has not searched for a city, show upcoming events from all cities
+    Given user has not searched for any city
     When the user opens the app
     Then the user should see the list of upcoming events
 
@@ -11,8 +11,9 @@ Feature: Filter events by city
     Then the user should receive a list of cities (suggestions) that match what they've typed
 
   Scenario: User can select a city from the suggested list
-    Given user was typing in "berlin" in the city textbox
+    # Given user was typing in "berlin" in the city textbox
+    Given the user was typing in berlin in the city textbox
     And the list of suggested cities is showing
-    When the user selects a city (e.g., "Berlin, Germany") from the list
-    Then their city should be changed to that city  (i.e., "Berlin, Germany")
-    And the user should receive a list of upcomning events in that city
+    When the user selects a city like Berlin, Germany from the list
+    Then their city should be changed to that city  Berlin, Germany
+    And the user should receive a list of upcoming events in that city
--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -9,8 +9,8 @@ import { extractLocations, locations } from '../api';
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
 defineFeature(feature, test => {
-  test('When user hasn\'t searched for a city, show upcoming ecvenrts from all cities', ({ given, when, then }) => {
-    given('user hasn\'t searched for any city', () => {
+  test('When user has not searched for a city, show upcoming events from all cities', ({ given, when, then }) => {
+    given('user has not searched for any city', () => {
 
     });
     let AppWrapper;
@@ -28,7 +28,7 @@ defineFeature(feature, test => {
 
     let CitySearchWrapper;
     given('the main page is open', () => {
-      CitySearchWrapper = shallow(<CitySearch /*updateEvents={() => { }} /*locations={locations}*/ />);
+      CitySearchWrapper = shallow(<CitySearch updateEvents={() => { }} locations={mockData.map((obj) => obj.location)} />);
     });
 
     when('the user starts typing in the city textbox', () => {
@@ -36,28 +36,96 @@ defineFeature(feature, test => {
     });
 
     then('the user should receive a list of cities (suggestions) that match what they\'ve typed', () => {
-      expect(CitySearchWrapper.find('suggestions li')).toHaveLength(2);
+      expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(2);
     });
   });
 
   test('User can select a city from the suggested list', ({ given, and, when, then }) => {
-    given(/^user was typing in "(.*)" in the city textbox$/, (arg0) => {
-
+    let AppWrapper;
+    given('the user was typing in berlin in the city textbox', async () => {
+      AppWrapper = await mount(<App />);
+      AppWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
     });
+
 
     and('the list of suggested cities is showing', () => {
-
+      AppWrapper.update();
+      expect(AppWrapper.find('.suggestions li')).toHaveLength(2);
     });
 
-    when(/^the user selects a city \(e.g., "(.*)"\) from the list$/, (arg0) => {
-
+    when('the user selects a city like Berlin, Germany from the list', () => {
+      AppWrapper.find('.suggestions li').at(0).simulate('click');
     });
 
-    then(/^their city should be changed to that city  \(i.e., "(.*)"\)$/, (arg0) => {
-
+    then('their city should be changed to that city  Berlin, Germany', () => {
+      const CitySearchWrapper = AppWrapper.find(CitySearch);
+      expect(CitySearchWrapper.state('query')).toBe('Berlin, Germany');
     });
-    and('the user should receive a list of upcomning events in that city', () => {
-
+    and('the user should receive a list of upcoming events in that city', () => {
+      expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
     });
   });
 });
+
+
+
+
+
+
+
+
+// defineFeature(feature, test => {
+//   test('When user has not searched for a city, show upcoming events from all cities', ({ given, when, then }) => {
+//     given('user has not searched for any city', () => {
+
+//     });
+//     let AppWrapper;
+//     when('the user opens the app', () => {
+//       AppWrapper = mount(<App />);
+//     });
+
+//     then('the user should see the list of upcoming events', () => {
+//       AppWrapper.update();
+//       expect(AppWrapper.find('.event')).toHaveLength(mockData.length);
+//     });
+//   });
+//   test('User should see a list of suggestions when they search for a city', ({ given, when, then }) => {
+//     let CitySearchWrapper;
+//     given('the main page is open', () => {
+//       CitySearchWrapper = shallow(<CitySearch updateEvents={() => { }} locations={locations} />);
+//     });
+
+//     when('the user starts typing in the city textbox', () => {
+//       CitySearchWrapper.find('.city').simulate('change', { target: { value: 'Berlin' } });
+
+//     });
+
+//     then('the user should receive a list of cities (suggestions) that match what they\'ve typed', () => {
+
+//     });
+//   });
+
+
+//   test('User can select a city from the suggested list', ({ given, and, when, then }) => {
+//     given('the user was typinh in berlin in thr city textbox', () => {
+
+//     });
+
+//     and('the list of suggested cities is showing', () => {
+
+//     });
+
+//     when('the user selects a city like Berlin, Germany from the list', () => {
+
+//     });
+
+//     then('their city should be changed to that city  Berlin, Germany', () => {
+
+//     });
+
+//     and('the user should receive a list of upcomning events in that city', () => {
+
+//     });
+//   });
+// });
+// // });
